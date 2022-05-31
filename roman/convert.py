fd23acabf69195754d1f8ad4d7e11e01551680c9
Sveinung Gundersen <sveinugu@gmail.com>
2022-05-31 14:17:09 +0200

    Suggestion from the "mob programming" session, with bugfix

--- roman/convert.py
+++ roman/convert.py
@@ -1,7 +1,12 @@
+decimal_to_roman_letter = {1: 'I', 5: 'V', 10: 'X'}
+
+
 def decimal_to_roman(number: int) -> str:
-    if number < 5:
-        return 'I' * number
-    elif number < 10:
-        return 'V' + 'I' * (number - 5)
-    else:
-        return 'X' + 'I' * (number - 10)
+    ret = ''
+
+    for i in reversed(sorted(decimal_to_roman_letter.keys())):
+        if number >= i:
+            ret += decimal_to_roman_letter[i] * (number // i)
+            number -= i
+
+    return ret
